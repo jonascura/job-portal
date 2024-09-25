f41468af1870bb797821ffd4499b05ca2ef257cb
Yiqun Huang Albert <albert913388672@gmail.com>
2024-09-24 17:54:41 -0700

    fix the index.js

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,37 +1,41 @@
 const express = require("express");
-const cors = require('cors');
-require('dotenv').config();
-const jobRoutes = require('./routes/jobRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const userRoutes = require('./routes/userRoutes');
-const videoRoutes = require('./routes/videoRoutes');
-const employerRoutes = require('./routes/employerRoutes');
-const errorHandler = require('./middleware/errorHandler');
+const cors = require("cors");
+require("dotenv").config();
+const jobRoutes = require("./routes/jobRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const userRoutes = require("./routes/userRoutes");
+const videoRoutes = require("./routes/videoRoutes");
+const employerRoutes = require("./routes/employerRoutes");
+const errorHandler = require("./middleware/errorHandler");
 const { admin, setAdminCustomClaims } = require("./config/firebaseAdmin");
-const { connectToMongo } = require('./config/mongoDB');
+const { connectToMongo } = require("./config/mongoDB");
 const fileRoutes = require("./routes/fileRoutes");
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// set emails recognized for admin claim
-const adminEmails = [
-  "admin@admin.com",
-];
+// Configure CORS options
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://job-portal-frontend-980c.onrender.com",
+  ], // Add your frontend URLs here
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // If you need to allow cookies or other credentials
+  optionsSuccessStatus: 200,
+};
 
 // middleware setup
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions)); // Use CORS with specific options
 
 // route handling
-app.use(jobRoutes); 
+app.use(jobRoutes);
 app.use(adminRoutes);
 app.use(userRoutes);
 app.use(videoRoutes);
 app.use(employerRoutes);
 app.use(fileRoutes);
-
 
 // default route
 app.get("/", (req, res) => {
@@ -42,15 +46,23 @@ app.get("/", (req, res) => {
 app.use(errorHandler); // Add the error handling middleware at the end
 
 // connect to mongoDB and set admin claims
-connectToMongo().then((connectedClient) => {
-  // set admin claims for Firebase
-  setAdminCustomClaims(adminEmails).catch(console.error);
+connectToMongo()
+  .then((connectedClient) => {
+    // set admin claims for Firebase
+    setAdminCustomClaims(adminEmails).catch(console.error);
 
-  // send a ping to confirm a successful connection
-  connectedClient.db("admin").command({ ping: 1 }).then(() => {
-    console.log("Pinged your deployment. You are successfully connected to MongoDB!");
-  }).catch(console.error);
-}).catch(console.error);
+    // send a ping to confirm a successful connection
+    connectedClient
+      .db("admin")
+      .command({ ping: 1 })
+      .then(() => {
+        console.log(
+          "Pinged your deployment. You are successfully connected to MongoDB!"
+        );
+      })
+      .catch(console.error);
+  })
+  .catch(console.error);
 
 // start the server
 app.listen(port, () => {
