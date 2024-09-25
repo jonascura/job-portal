9f958cc33831a2f15d4c1eb027e4207f597ab06e
Jonas Cura <jonasm.cura@gmail.com>
2024-09-25 14:21:43 -0600

    create JobPreview window

--- job-portal-client/src/Pages/Home.jsx
+++ job-portal-client/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Card from "../component/Card";
 import VideoList from "../component/VideoList";
 import Jobs from "./Jobs";
 import Sidebar from "../sidebar/Sidebar";
+import JobPreview from "../component/JobPreview";
 
 const Home = () => {
   // States for job data and UI control
@@ -11,74 +12,11 @@ const Home = () => {
   const [selectedSalary, setSelectedSalary] = useState("All");
   const [selectedEmploymentType, setSelectedEmploymentType] = useState("All");
   const [selectedDate, setSelectedDate] = useState("All time");
+  const [selectedJob, setSelectedJob] = useState(null);
   const [jobs, setJobs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
-  const [videos, setVideos] = useState([]); // setting videos can improve load time
-
-  // Fetching videos
-  // const fetchVideos = async () => {
-  //   // Fetching videos from the MongoDB
-  //   const mongoResponse = await fetch("http://localhost:3000/all-videos");
-  //   const mongoVideos = mongoResponse.ok ? await mongoResponse.json() : [];
-    
-
-  //   // Fetching videos from S3
-  //   const s3Response = await fetch("http://localhost:3000/s3-videos");
-  //   const s3Videos = s3Response.ok ? await s3Response.json() : [];
-  //   const formattedS3Videos = s3Videos.map((url, index) => ({
-  //     id: `s3-${index}`, // Creating a unique ID for each S3 video
-  //     url,
-  //   }));
-
-    // Combine both video arrays
-    // const combinedVideos = [...formattedS3Videos, ...mongoVideos];
-
-    // Update state with combined video list
-  //   setVideos(combinedVideos);
-
-  //   if (!mongoResponse.ok || !s3Response.ok) {
-  //     console.error("Failed to fetch videos from one or both sources");
-  //   }
-  // };
-
-
-  // useEffect(() => {
-  //   fetchVideos();
-  // }, []); // Ensure this runs only once when the component mounts
-
-  // Enhanced version with feedback and error handling
-  // const handleVideoUpload = async (event) => {
-  //   const file = event.target.files[0];
-  //   console.log(file.type); // Log the MIME type of the file
-  //   if (!file) return;
-
-  //   if (!file.type.startsWith("video/")) {
-  //     alert("Please select a valid video file.");
-  //     return;
-  //   }
-
-  //   const formData = new FormData();
-  //   formData.append("file", file);
-
-  //   setIsLoading(true); // Show loading indicator during upload
-  //   try {
-  //     const response = await fetch("http://localhost:3000/upload", {
-  //       method: "POST",
-  //       body: formData,
-  //     });
-  //     if (!response.ok) throw new Error("Upload failed");
-  //     const data = await response.json();
-  //     setVideos([...videos, data]); // Update the video list with the new video
-  //     alert("Video uploaded successfully!");
-  //   } catch (error) {
-  //     console.error("Error uploading video:", error);
-  //     alert("Failed to upload video."); // User-friendly error message
-  //   } finally {
-  //     setIsLoading(false); // Hide loading indicator regardless of the outcome
-  //   }
-  // };
 
   // Fetching job listings
   useEffect(() => {
@@ -98,17 +36,6 @@ const Home = () => {
         setIsLoading(false);
     });
   },[]);
-
-  // get videos
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   fetch(`http://localhost:3000/all-videos`)
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     setVideos(data);
-  //     setIsLoading(false);
-  //   });
-  // },[]);
 
   // Input change handlers for each filter
   const handleProvinceChange = (province) => setSelectedProvince(province);
@@ -166,7 +93,11 @@ const Home = () => {
   // Render filtered jobs as card components
   const jobCards = filteredJobs
     .slice(...Object.values(calculatePageRange()))
-    .map((job, index) => <Card key={index} data={job} />);
+    .map((job, index) => (
+      <div key={index} onClick={() => setSelectedJob(job)}>
+        <Card data={job}/>
+      </div>
+  ));
 
   return (
     <div>
@@ -176,9 +107,9 @@ const Home = () => {
       />
 
       {/* main content */}
-      <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
-        {/* Left side for Sidebar (1/5 of total width) */}
-        <div className="bg-white p-4 rounded-2xl">
+      <div className="bg-[#FAFAFA] md:grid grid-cols-12 gap-8 lg:px-24 px-4 py-12">
+        {/* Left side for Sidebar (2/12 of total width) */}
+        <div className="col-span-2 bg-white p-4 rounded-2xl">
           <Sidebar
             handleProvinceChange={handleProvinceChange}
             handleSalaryChange={handleSalaryChange}
@@ -187,8 +118,8 @@ const Home = () => {
           />
         </div>
 
-        {/* Middle */}
-        <div className="col-span-2 bg-white p-4 rounded-2xl">
+        {/* Middle column (6/12 of total width) */}
+        <div className="col-span-6 bg-white p-4 rounded-2xl">
           {isLoading ? (
             <p>Loading...</p>
           ) : jobCards.length ? (
@@ -224,13 +155,12 @@ const Home = () => {
           ) : null}
         </div>
 
-        {/* Right */}
-        <div className="bg-white p-4 rounded-2xl">
+        {/* Job preview on the right side (4/12 of total width) */}
+        <div className="col-span-4 bg-white p-4 rounded-2xl sticky right-20 top-24">
+          <JobPreview job={selectedJob} />
         </div>
-
-        
-
       </div>
+
     </div>
   );
 };
